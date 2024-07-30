fcdc874205bb912fe4606f54b7e0f97f03041a5a
YuriiMololodtsov <88619986+YuriiMololodtsov@users.noreply.github.com>
2024-07-30 17:47:24 +0300

    fix: fixed build files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,27 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-      prettier: {
-        files: {
-          src: ['src/**/*.{js,jsx,ts,tsx,json}'],
-        },
-      },
-    });
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-    grunt.loadNpmTasks('grunt-prettier');
-    grunt.registerTask('default', ['prettier']);
-  };
+    prettier: {
+      files: {
+        src: ['src/**/*.{js,jsx,ts,tsx,json}'],
+      },
+    },
+
+    'gh-pages': {
+      options: {
+        base: 'build',
+      },
+      src: ['**/*']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-prettier');
+  grunt.loadNpmTasks('grunt-gh-pages');
+
+  grunt.registerTask('default', ['prettier']);
+
+  grunt.registerTask('build', ['prettier']);
+
+  grunt.registerTask('deploy', ['build', 'gh-pages']);
+};
